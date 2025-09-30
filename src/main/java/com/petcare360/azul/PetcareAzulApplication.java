0abff7982d8709790c697007c14c18c2852c9d5f
Juan Andres <Juan.sfonesca@mail.escuelaing.edu.co>
2025-09-30 10:15:18 -0500

    feat:Crear el scaffolding del proyecto con Spring Boot y gestión con Maven

---
+++ src/main/java/com/petcare360/azul/PetcareAzulApplication.java
@@ -0,0 +1,13 @@
+package com.petcare360.azul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetcareAzulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetcareAzulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/petcare360/azul/PetcareAzulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.petcare360.azul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetcareAzulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
